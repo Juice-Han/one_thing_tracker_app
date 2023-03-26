c9279792ce1aa35e2dc0258016be1b168caab1f6
JuHan <iiiamst16@gmail.com>
2023-03-26 14:19:59 +0900

    add setting details

--- lib/components/Setting.dart
+++ lib/components/Setting.dart
@@ -1,20 +1,39 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:one_thing_tracker_app/main.dart';
 import 'package:provider/provider.dart';
 
 import '../notification.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   Setting({super.key, this.changeOneThing});
 
+  var changeOneThing;
+
+  @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
   final textController1 = TextEditingController();
   final textController2 = TextEditingController();
+  var saveIndex = 0;
 
-  var changeOneThing;
+  @override
+  void initState() {
+    super.initState();
+    textController1.addListener(() {
+      if (textController1.text.isNotEmpty) {
+        setState(() {
+          saveIndex = 1;
+        });
+      } else {
+        setState(() {
+          saveIndex = 0;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +45,13 @@ class Setting extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Center(
             child: SingleChildScrollView(
-              child: Container(
-                height: MediaQuery.of(context).size.height*9/10,
-                child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
+          child: Container(
+            height: MediaQuery.of(context).size.height * 9 / 10,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
                 Text(
-                  '당신의 OneThing은 무엇인가요?',
+                  '당신의 OneThing은 무엇인가요? (필수)',
                   style: TextStyle(fontSize: 20),
                 ),
                 TextField(
@@ -40,24 +59,31 @@ class Setting extends StatelessWidget {
                   controller: textController1,
                 ),
                 Text(
-                  '매일 정해진 시간에 알림을 전송할까요?',
-                  style: TextStyle(fontSize: 18),
+                  '매일 정해진 시간에 알림을 전송할까요? (선택)',
+                  style: TextStyle(fontSize: 17),
                 ),
                 TimePick(),
                 SizedBox(
                   height: 6,
                 ),
-                ElevatedButton(
+                [
+                  ElevatedButton(
+                    onPressed: null,
+                    child: Text('원띵을 정해주세요'),
+                  ),
+                  ElevatedButton(
                     onPressed: () {
                       Navigator.pop(context);
-                      changeOneThing(textController1.text);
+                      widget.changeOneThing(textController1.text);
                       context.read<store1>().saveOneThing(textController1.text);
                     },
-                    child: Text('저장'))
-                        ],
-                      ),
-              ),
-            )),
+                    child: Text('저장'),
+                  ),
+                ][saveIndex]
+              ],
+            ),
+          ),
+        )),
       ),
     );
   }
@@ -72,20 +98,27 @@ class TimePick extends StatefulWidget {
 
 class _TimePickState extends State<TimePick> {
   var index = 0;
+  var selectedTime;
   @override
   Widget build(BuildContext context) {
     return [
       ElevatedButton(
           onPressed: () {
-            Future<TimeOfDay?> selectedTime =
+            selectedTime =
                 showTimePicker(context: context, initialTime: TimeOfDay.now());
-            selectedTime.then((timeOfDay) =>
-                context.read<store1>().changeNotificationTime(timeOfDay));
-            setState(() {
-              index = 1;
+            selectedTime.then((timeOfDay) {
+              if (timeOfDay == null) {
+                selectedTime = null;
+                showDialog(context: context, builder: (context) => AlertDialog(title: Text('주의!'),content: Text('시간이 선택되지 않았습니다',)));
+              } else {
+                context.read<store1>().changeNotificationTime(timeOfDay);
+                setState(() {
+                  index = 1;
+                });
+              }
             });
           },
-          child: Text('시간 정하기')),
+          child: Text('시간을 정해주세요')),
       ElevatedButton(
           onPressed: () {
             FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -96,7 +129,7 @@ class _TimePickState extends State<TimePick> {
                 ?.requestPermission();
             showNotification(context.read<store1>().notificationTime);
           },
-          child: Text('알림 권한 허용')),
+          child: Text('버튼을 누르고 알림 권한을 허용해주세요')),
     ][index];
   }
 }
