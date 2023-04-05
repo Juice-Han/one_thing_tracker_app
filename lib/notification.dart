25dbe4fdedd7b7fd10ccecb08dc551e9948cbbff
JuHan <iiiamst16@gmail.com>
2023-04-06 00:23:03 +0900

    fix notification icon & bug

--- lib/notification.dart
+++ lib/notification.dart
@@ -8,7 +8,7 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 initNotification() async {
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('ic_notification');
+      AndroidInitializationSettings('@mipmap/ic_launcher');
 
   final InitializationSettings initializationSettings =
       InitializationSettings(android: initializationSettingsAndroid);
